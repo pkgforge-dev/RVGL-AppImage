36bd5d30cb126e497615746e662f9a7c6b039379
Link4Electronics <link4electronics@gmail.com>
2026-03-15 16:37:15 -0300

    improve script and hook

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,32 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    enet             \
+    fluidsynth       \
+    openal           \
+    python-packaging \
+    python-requests  \
+    python-wxpython
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+#make-aur-package flac1.4
 
-# If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+wget https://rvgl.org/downloads/rvgl_launcher_linux.zip
+wget https://distribute.re-volt.io/packs/rvgl_linux.zip
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+bsdtar -xvf rvgl_launcher_linux.zip -C ./AppDir/bin
+case "$ARCH" in # they use 64 and arm64
+	x86_64)  farch=64;;
+	aarch64) farch=arm64;;
+esac
+bsdtar -xvf rvgl_linux.zip -C ./AppDir/bin rvgl.${farch}
+bsdtar -xvf rvgl_linux.zip -C /usr/lib --strip-components 2 lib/lib${farch}/libunistring.so.2
+rm -rf ./AppDir/bin/icons
