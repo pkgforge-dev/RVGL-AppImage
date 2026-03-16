a79b60c559a66b9d01ce36ee3bb460bfa1f17145
Link4Electronics <link4electronics@gmail.com>
2026-03-15 23:00:00 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,6 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DEPLOY_GTK=1
 export DEPLOY_OPENGL=1
 export DEPLOY_PYTHON=1
 
