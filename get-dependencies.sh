85df33c4f4ca8a08264e223366e1bc94deeeb8d0
Link4Electronics <link4electronics@gmail.com>
2026-03-16 02:12:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,8 +34,10 @@ wget https://distribute.re-volt.io/packs/rvgl_linux.zip
 mkdir -p ./AppDir/bin
 bsdtar -xvf rvgl_launcher_linux.zip -C ./AppDir/bin
 if [ "$ARCH" = "x86_64" ]; then
-    bsdtar -xvf rvgl_linux.zip -C ./AppDir/bin rvgl.64
+    bsdtar -xvf rvgl_linux.zip -C ./AppDir/bin lib/lib64/rvgl.64
+    bsdtar -xvf rvgl_linux.zip -C /usr/lib --strip-components 2 lib/lib64/libunistring.so.2
 else
     bsdtar -xvf rvgl_linux.zip -C ./AppDir/bin rvgl.arm64
+    bsdtar -xvf rvgl_linux.zip -C /usr/lib --strip-components 2 lib/libarm64/libunistring.so.2
 fi
 rm -rf ./AppDir/bin/icons
